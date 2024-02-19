e762d652c8e7b1d5bb6caf3bff845f8bb5e53709
Carlos <cgmartinezo@gmail.com>
2024-02-18 23:53:54 -0300

    commit 2

---
+++ assets/js/sript.js
@@ -0,0 +1,12 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+// Añadir un evento click al botón
+$("#btnEnviar").click(function () {
+    alert("¡Mensaje enviado con éxito!");
+});
